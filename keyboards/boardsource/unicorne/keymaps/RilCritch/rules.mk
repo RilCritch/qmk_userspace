92ec02c70086f26a605fd8f10662bd701ec68a83
RilCritch <rilcritch@gmail.com>
2024-01-07 16:51:41 -0600

    working on unicorne firmware

---
+++ keyboards/boardsource/unicorne/keymaps/RilCritch/rules.mk
@@ -0,0 +1,12 @@
+#_( Unicorne Default = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =>
+SERIAL_DRIVER = vendor
+AUDIO_DRIVER = pwm_hardware
+POINTING_DEVICE_DRIVER = analog_joystick
+
+
+#_( Enabling = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =>
+## Software Features
+CAPS_WORD_ENABLE = yes
+
+
+#_( Disabled = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =>

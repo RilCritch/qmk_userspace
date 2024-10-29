efd5437a47e4484e22874e4f6d185a1441402fec
RilCritch <rilcritch@gmail.com>
2024-10-29 17:29:40 -0500

    Tweaking the unicorne config

--- keyboards/boardsource/unicorne/keymaps/RilCritch/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/RilCritch/keymap.c
@@ -86,7 +86,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
 
 //= - Global Override Definitions ->
-const key_override_t **key_overrides = (const key_override_t *[]){
+const key_override_t *key_overrides[] = {
     &delete_key_override,
-    NULL // Null terminate the array of overrides!
 };
